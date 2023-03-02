a4e7f2e99dd7709bd6eea05937afb677beadf211
Adeleadbe <adbe21@gmail.com>
2023-03-02 11:26:38 +0100

    Reprise du Portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const navAbout = document.querySelector('.banner_navbar_about');
+const navProject = document.querySelector('.banner_navbar_project');
+const navbarAbout = document.querySelector('.about');
+const navbarProject = document.querySelector('.project');
+let heightAbout = navbarAbout.clientHeight;
+let heightProject = navbarProject.clientHeight;
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > heightAbout) {
+        navAbout.classList.add('scroll');
+    } else if(window.scrollY > heightProject) {
+        navProject.classList.add('scroll');
+    } else {
+        navAbout.classList.remove('scroll');
+    }
+
+    if (window.scrollY > heightProject) {
+        navAbout.classList.remove('scroll');
+    }
+}); */
