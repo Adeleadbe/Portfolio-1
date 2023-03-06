8570c10c689047278720356dcf7c759c3a519fe3
Adeleadbe <adbe21@gmail.com>
2023-03-06 17:40:53 +0100

    Mise en place de l'animation des projets

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ window.addEventListener("scroll", () => {
         navAbout.classList.remove('scroll');
     }
 }); */
+
+function animateElements(selector, numberHeight) {
+    const elements = document.querySelectorAll(selector);
+
+    window.addEventListener("scroll", () => {
+        const { scrollTop, clientHeight } = document.documentElement;
+
+        elements.forEach((element) => {
+            const topElementToTopViewport = element.getBoundingClientRect().top;
+            // Si ce que j'ai scrollé depuis le haut de mon element est supérieur à la hauteur de mon element et le haut de ma partie visible (tofixed enleve les chiffres apres vurgule)
+            if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * numberHeight) {
+                element.classList.add("active");
+            }
+        })
+    });
+}
+
+animateElements(".project_animation", 0.8)
+animateElements(".project_content_text_animation", 1)
