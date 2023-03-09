4e0dfb29f4a5a639174eaf37b450b0c5bf195b45
Adeleadbe <adbe21@gmail.com>
2023-03-09 16:49:04 +0100

    responsive terminé

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-/*const navAbout = document.querySelector('.banner_navbar_about');
-const navProject = document.querySelector('.banner_navbar_project');
-const navbarAbout = document.querySelector('.about');
-const navbarProject = document.querySelector('.project');
-let heightAbout = navbarAbout.clientHeight;
-let heightProject = navbarProject.clientHeight;
-
-window.addEventListener("scroll", () => {
-    if(window.scrollY > heightAbout) {
-        navAbout.classList.add('scroll');
-    } else if(window.scrollY > heightProject) {
-        navProject.classList.add('scroll');
-    } else {
-        navAbout.classList.remove('scroll');
-    }
-
-    if (window.scrollY > heightProject) {
-        navAbout.classList.remove('scroll');
-    }
-}); */
-
-function animateElements(selector, numberHeight) {
+function animateElements(selector) {
     const elements = document.querySelectorAll(selector);
 
     window.addEventListener("scroll", () => {
@@ -28,12 +7,12 @@ function animateElements(selector, numberHeight) {
         elements.forEach((element) => {
             const topElementToTopViewport = element.getBoundingClientRect().top;
             // Si ce que j'ai scrollé depuis le haut de mon element est supérieur à la hauteur de mon element et le haut de ma partie visible (tofixed enleve les chiffres apres vurgule)
-            if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * numberHeight) {
+            if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.9) {
                 element.classList.add("active");
             }
         })
     });
 }
 
-animateElements(".project_animation", 0.8)
-animateElements(".project_content_text_animation", 1)
+animateElements(".project_animation")
+animateElements(".project_content_text_animation")
